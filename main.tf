faf2a19ad9db6d2a4a692c6da828b7ed970602d5
jagathda <mail.jagath@gmail.com>
2024-09-05 13:01:05 +0300

    Added security group

--- main.tf
+++ main.tf
@@ -47,3 +47,26 @@ resource "aws_route_table_association" "public_subnet_2_association" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+# Create a security group for the Fargate service
+resource "aws_security_group" "fargate_sg" {
+  vpc_id = aws_vpc.fargate_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "FargateSecurityGroup"
+  }
+}
