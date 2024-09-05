39e64fd14f8da7a6cbb238a88279969ac4637462
jagathda <mail.jagath@gmail.com>
2024-09-05 12:55:42 +0300

    Added public route table associations

--- main.tf
+++ main.tf
@@ -36,3 +36,14 @@ resource "aws_route_table" "public_route_table" {
     gateway_id = aws_internet_gateway.fargate_igw.id
   }
 }
+
+# Associate route table with subnets
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
