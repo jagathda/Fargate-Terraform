42b238fc0ddda1a58c56ab70778a1f918e191fc3
jagathda <mail.jagath@gmail.com>
2024-09-05 12:40:29 +0300

    Added VPC

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = var.region
 }
+
+# Create a VPC
+resource "aws_vpc" "fargate_vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "FargateVPC"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   description = "AWS region"
   default     = "eu-north-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
