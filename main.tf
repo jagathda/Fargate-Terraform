ea8fdeebc59257c66c56424334d32f63610344e8
jagathda <mail.jagath@gmail.com>
2024-09-05 12:43:44 +0300

    Added public subnets

--- main.tf
+++ main.tf
@@ -9,3 +9,17 @@ resource "aws_vpc" "fargate_vpc" {
     Name = "FargateVPC"
   }
 }
+
+# Create public subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.fargate_vpc.id
+  cidr_block        = var.public_subnet_cidr_1
+  availability_zone = "${var.region}a"
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.fargate_vpc.id
+  cidr_block        = var.public_subnet_cidr_2
+  availability_zone = "${var.region}b"
+}
+
--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnet_cidr_1" {
+  description = "Public subnet CIDR block 1"
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_cidr_2" {
+  description = "Public subnet CIDR block 2"
+  default     = "10.0.2.0/24"
+}
